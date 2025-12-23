7bddf0f13bc0694e94a171034b06c34667d2427f
jirattikan-pk <mindmook.2006@gmail.com>
2025-12-23 03:08:05 -0600

    cp10-3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+    ifstream source("score.txt");
+    string txt;
+    int count=0;
+    double num=0.0,N=0.0;
+
+    while (getline(source,txt)){
+        num += stod(txt);
+        count++;
+        N += pow(stod(txt),2);
+    }
+
+    double mean = num/count;
+    double SD = sqrt((N/count)-(mean*mean));
+
+    cout << "Number of data = " << count <<"\n";
+    cout << setprecision(3);
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << SD ;
+}
