d1ab4e3037cd807e9039f5a81238aa0322717ddd
jirattikan-pk <mindmook.2006@gmail.com>
2025-12-23 02:36:51 -0600

    cp10-2

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,11 +6,15 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
+	string txt;
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	dest << "-------------------- BOOM ---------------------\n";
+	while (getline(source,txt)){
+		dest << txt <<"\n";
+	}
+	dest << "-------------------- HA!! ---------------------";
 
     source.close();
     dest.close();
